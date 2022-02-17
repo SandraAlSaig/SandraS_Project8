bea8800f7ed5dc284a99d0cfe5f93d59958f674a
SandraS <alsaigsandra@gmail.com>
2022-02-17 10:48:21 +0100

    Created JS file.

---
+++ Emplyee Directory.js
@@ -0,0 +1,8 @@
+
+
+let employees =[];
+const urlAPI = ``;
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector("modal-close");
