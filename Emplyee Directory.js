22c69b8266e8bf92f104ef381df0349a161627d2
SandraS <alsaigsandra@gmail.com>
2022-02-17 13:13:42 +0100

    Added Normalise.css and updated html linked files

--- Emplyee Directory.js
+++
@@ -1,8 +0,0 @@
-
-
-let employees =[];
-const urlAPI = ``;
-const gridContainer = document.querySelector(".grid-container");
-const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
-const modalClose = document.querySelector("modal-close");
